6f23fcc6a32762ef2ae761411215d6a5946755ea
ksenci8 <ksencilo88@gmail.com>
2025-12-30 22:28:45 -0500

    Fix vite.config back for Vercel deployment not GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
-  base: "/Web-Development-/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
-})
+});
